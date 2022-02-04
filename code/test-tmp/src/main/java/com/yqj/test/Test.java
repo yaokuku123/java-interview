46054a1eb61ef866b368247326049bdf640ea409
yaokuku123 <yaoqijun@outlook.com>
2022-02-04 14:48:11 +0800

    update base

--- code/test-tmp/src/main/java/com/yqj/test/Test.java
+++ code/test-tmp/src/main/java/com/yqj/test/Test.java
@@ -1,10 +1,20 @@
 package com.yqj.test;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int[][] nums = new int[][]{{1, 2},{3,4}};
+        Arrays.sort(nums,(num1,num2) -> {
+            if (num1[0] == num2[0]) {
+                return num1[1] - num2[1];
+            } else {
+                return num2[0] - num1[0];
+            }
+        });
+        Arrays.stream(nums).forEach((o)-> System.out.print(o + " "));
     }
 }
